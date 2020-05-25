3c43ce052ac777c2096b6516025665e0c0ff7ee5
Ignacio Donovan <ignacio.donovan@xero.com>
2020-05-26 01:10:47 +1200

    Pagination

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 import * as movieService from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "./common/utils/paginate";
 
 class Movies extends Component {
   state = {
     movies: [],
+    currentPage: 1,
+    pageSize: 4,
   };
   constructor() {
     super();
@@ -25,28 +29,10 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
-  getMovies = () => {
-    return this.state.movies.map((movie) => (
-      <tr key={movie._id}>
-        <td>{movie.title}</td>
-        <td>{movie.genre.name}</td>
-        <td>{movie.numberInStock}</td>
-        <td>{movie.dailyRentalRate}</td>
-        <td>
-          <Like liked={movie.liked} onToggle={() => this.handleToggle(movie)} />
-        </td>
-        <td>
-          <button
-            onClick={() => this.handleDelete(movie)}
-            type="button"
-            className="btn btn-danger btn-sm"
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
-    ));
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
   };
+
   getColumns = () => {
     const columns = ["Title", "Genre", "Stock", "Rate", "-", ""];
     return columns.map((col) => (
@@ -60,6 +46,10 @@ class Movies extends Component {
     const { length: movieCount } = this.state.movies;
     if (movieCount === 0) return <h3>There are no movies in the database.</h3>;
 
+    const { currentPage, pageSize, movies: allMovies } = this.state;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
+
     return (
       <>
         <h2>showing {movieCount} movies in the database</h2>
@@ -67,8 +57,38 @@ class Movies extends Component {
           <thead>
             <tr>{this.getColumns()}</tr>
           </thead>
-          <tbody>{this.getMovies()}</tbody>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onToggle={() => this.handleToggle(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    type="button"
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
+        <Pagination
+          currentPage={currentPage}
+          itemsCount={movieCount}
+          onPageChange={this.handlePageChange}
+          pageSize={pageSize}
+        />
       </>
     );
   }

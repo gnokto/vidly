108c02553039963a5ab61f48c5a7b6b26a9c077f
Ignacio Donovan <ignacio.donovan@xero.com>
2020-05-30 17:47:24 +1200

    App_Input stateless component

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import Input from "./common/input";
+
 class LoginForm extends Component {
   state = { account: { username: "", password: "" } };
 
@@ -20,28 +22,20 @@ class LoginForm extends Component {
       <div>
         <h1>Login Form</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              value={account.username}
-              onChange={this.handleChange}
-              name="username"
-              id="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              name="password"
-              id="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+          <Input
+            autoFocus
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={this.handleChange}
+          />
+          <Input
+            autoFocus
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={this.handleChange}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>

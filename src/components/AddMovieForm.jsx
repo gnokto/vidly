0a5bdebcdd3fc5092aa06a14df688bb08271d98b
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-01 14:40:19 +1200

    Add MovieForm

---
+++ src/components/AddMovieForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class AddMovieForm extends Component {
+  state = {};
+  render() {
+    return <h1>New Movie</h1>;
+  }
+}
+
+export default AddMovieForm;

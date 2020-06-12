b62e889fa3896ab55d0ae76d85fc57f477d14922
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-12 14:54:46 +1200

    Movie Form uses API now

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,17 +1,26 @@
 import http from "./httpService";
 import config from "../config.json";
 
+const apiEndpoint = config.apiBaseEndpoint + "/movies";
+
 export function getMovies() {
-  return http.get(config.apiBaseEndpoint + "/movies");
+  return http.get(apiEndpoint);
 }
 export function getMovie(movieId) {
-  return http.get(config.apiBaseEndpoint + "/movies/" + movieId);
+  return http.get(apiEndpoint + "/" + movieId);
 }
 
 export function saveMovie(movie) {
-  // return http.put(config.apiBaseEndpoint + "/movies/", movie);
+  //  if movieId exist update movie
+  //  otherwise add a new movie
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(apiEndpoint + "/" + movie._id, body);
+  }
+  return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(config.apiBaseEndpoint + "/movies/" + movieId);
+  return http.delete(apiEndpoint + "/" + movieId);
 }
